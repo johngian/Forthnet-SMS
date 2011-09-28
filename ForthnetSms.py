7768a36455b8cbd7eb4b716869726860a1960aaa
John Giannelos <johngiannelos@gmail.com>
2011-09-28 18:11:55 +0300

    Initial commit

---
+++ ForthnetSms.py
@@ -0,0 +1,49 @@
+import mechanize
+import cookielib
+
+br = mechanize.Browser()
+
+cj = cookielib.LWPCookieJar()
+br.set_cookiejar(cj)
+
+br.set_handle_equiv(True)
+br.set_handle_gzip(True)
+br.set_handle_redirect(True)
+br.set_handle_referer(True)
+br.set_handle_robots(False)
+
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+
+br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+
+r = br.open('https://www.forthnet.gr/secure/webSMS/default.aspx')
+html = r.read()
+
+
+#Site-authentication (email/password)
+
+br.select_form(nr=0)
+
+print "Email:"
+msg = raw_input()
+br.form['Username'] = msg
+
+print "Password:"
+password = raw_input()
+br.form['Password'] = password
+
+br.submit()
+
+#Send sms
+br.select_form(nr=0)
+
+print "Phone Number:"
+number = raw_input()
+br.form['txtTo'] = number
+
+print "Sms Text:"
+message = raw_input()
+br.form['txtMessage'] = message
+
+br.submit()
+
